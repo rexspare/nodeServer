b22fa4c662340f39885142f93a8a0ad5daeecf4d
Hamza Zafar <irexspare@gmail.com>
2023-07-21 17:41:37 +0500

    add enable CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 // Cross Origin Resource Sharing
 
 const allowedOrigins = [
-    'https://www.yoursite.com',
+    'https://www.oracions.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'https://gold-hungry-pelican.cyclic.app',
