eed4d9823ee37ae547dc7e4a819cd3f66e119f73
Hamza Zafar <irexspare@gmail.com>
2023-07-21 18:02:58 +0500

    add enable CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -5,7 +5,7 @@ const allowedOrigins = [
     'https://www.oracions.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'https://gold-hungry-pelican.cyclic.app',
+    'https://pear-easy-duck.cyclic.app',
     'https://64ba7c0ae9e426052964a832--sparkling-gecko-b6f2e1.netlify.app/'
 ]
 
