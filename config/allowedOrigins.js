b7ae11d815766706d9a6ba2755affc3b4e7f1ba5
Hamza Zafar <irexspare@gmail.com>
2023-07-21 17:38:49 +0500

    add enable CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -5,7 +5,8 @@ const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'https://gold-hungry-pelican.cyclic.app'
+    'https://gold-hungry-pelican.cyclic.app',
+    'https://64ba7c0ae9e426052964a832--sparkling-gecko-b6f2e1.netlify.app/'
 ]
 
 
